069f7dd9e8f77d436c43831a9947984bac2eeecc
Andika <andikasupriyadi27@gmail.com>
2024-09-12 09:52:38 +0700

    update

--- resources/views/myskill/partials/navbar.blade.php
+++ resources/views/myskill/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav id="navbar"
     class="sticky top-0 px-8 flex justify-between items-center bg-orange-400 z-10 w-screen transition-all duration-300 ease-in-out">
     <a class="text-2xl font-bold leading-none" href="#">
-        <img class="max-w-20 max-md:w-20 mx-10 h-auto" src="{{ asset('assets/logo.png') }}" alt="logo-company">
+        <img class="max-w-20 max-sm:mx-0 max-md:w-20 mx-10 h-auto" src="{{ asset('assets/logo.png') }}" alt="logo-company">
     </a>
     <div class="lg:hidden">
         <button class="navbar-burger flex items-center p-3 focus:outline-none hover:text-white focus:text-gray-900">
@@ -51,7 +51,7 @@
             </a>
         </li>
     </ul>
-    <div class="max- hidden lg:block space-x-2 justify-items-end mt-1.5">
+    <div class="max-md:hidden lg:block space-x-2 justify-items-end mt-1.5">
         @if (Auth::check())
             <div class="bg-transparent flex justify-center items-center mx-5">
                 <div x-data="{ open: false }" class="bg-transparent w-40 flex justify-center items-center">
